1b95d04ed69214b309bf9579b54a9e8df1e10cd5
DevinW <devinswilson03@gmail.com>
2024-11-01 14:05:49 -0400

    made parent contract class, added code inside

---
+++ src/main/java/com/pluralsight/Contract.java
@@ -0,0 +1,52 @@
+package com.pluralsight;
+
+public abstract class Contract {
+    private String dateOfContract;
+    private String customerName;
+    private String customerEmail;
+    private String vehicleSold;
+    private double totalPrice;
+    private double monthlyPayment;
+
+    public Contract(String dateOfContract, String customerName, String customerEmail, String vehicleSold) {
+        this.dateOfContract = dateOfContract;
+        this.customerName = customerName;
+        this.customerEmail = customerEmail;
+        this.vehicleSold = vehicleSold;
+    }
+
+    public String getDateOfContract() {
+        return dateOfContract;
+    }
+
+    public void setDateOfContract(String dateOfContract) {
+        this.dateOfContract = dateOfContract;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+
+    public void setCustomerEmail(String customerEmail) {
+        this.customerEmail = customerEmail;
+    }
+
+    public String getVehicleSold() {
+        return vehicleSold;
+    }
+
+    public void setVehicleSold(String vehicleSold) {
+        this.vehicleSold = vehicleSold;
+    }
+
+    public void getTotalPrice(){}
+    public void getMonthlyPayment(){}
+}
